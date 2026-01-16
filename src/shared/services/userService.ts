16bb8260c58349fbf1c303e12c82169d11fa9525
Joan Matias <yoan16908@gmail.com>
2026-01-15 22:13:33 -0500

    Update userService.ts

--- src/shared/services/userService.ts
+++ src/shared/services/userService.ts
@@ -1,10 +1,12 @@
 import {type UserFormData } from '@/shared/schemas/UserSchema';
 import { getAuthToken } from '../utils/getAuthToken';
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
 export const userService = {
   async registerUser(data: UserFormData): Promise<{ id: string }> {
     try {
-    const response = await fetch(`/api/register?token=${getAuthToken()}`, {
+    const response = await fetch(`${BACKEND_URL}/register?token=${getAuthToken()}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
